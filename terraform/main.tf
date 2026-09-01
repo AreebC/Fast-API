ec6785c91885237798161d41abc5126bec78b51d
Areeb Chowdhury <areebchowdhury05@gmail.com>
2026-09-01 18:09:19 +0100

    Correctly formatted files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,85 +1,85 @@
 module "s3" {
-    source = "./modules/s3"
-    bucket_name1 = var.bucket_name1
-    bucket_name2 = var.bucket_name2
-    bucket_name3 = var.bucket_name3
+  source       = "./modules/s3"
+  bucket_name1 = var.bucket_name1
+  bucket_name2 = var.bucket_name2
+  bucket_name3 = var.bucket_name3
 }
 
 module "vpc" {
-    source = "./modules/vpc"
-    cidr_block = var.cidr_block
-    vpc_name = var.vpc_name
-    env = var.env
-    subnet_cidr_block = var.subnet_cidr_block
-    subnet_cidr_block2 = var.subnet_cidr_block2
-    private_subnet_cidr_block = var.private_subnet_cidr_block
-    private_subnet_cidr_block2 = var.private_subnet_cidr_block2
-    availability_zone = var.availability_zone
-    availability_zone2 = var.availability_zone2
+  source                     = "./modules/vpc"
+  cidr_block                 = var.cidr_block
+  vpc_name                   = var.vpc_name
+  env                        = var.env
+  subnet_cidr_block          = var.subnet_cidr_block
+  subnet_cidr_block2         = var.subnet_cidr_block2
+  private_subnet_cidr_block  = var.private_subnet_cidr_block
+  private_subnet_cidr_block2 = var.private_subnet_cidr_block2
+  availability_zone          = var.availability_zone
+  availability_zone2         = var.availability_zone2
 }
 
 module "eks" {
-    source = "./modules/eks"
-    depends_on = [ module.vpc ]
-    cluster_name = var.cluster_name
-    tags = var.tags
-    eks_cluster_role_arn = module.iam-cluster.eks_cluster_role_arn
-    eks_nodegroup_role_arn = module.iam-cluster.eks_nodegroup_role_arn
-    vpc_id = module.vpc.vpc_id
-    subnet_ids = flatten([module.vpc.public_subnet_ids, module.vpc.private_subnet_ids])
-    node_group_desired_size = var.node_group_desired_size
-    node_group_max_size = var.node_group_max_size
-    node_group_min_size = var.node_group_min_size
-    node_group_instance_types = var.node_group_instance_types
-    private_subnet_cidrs = module.vpc.private_subnet_cidrs
+  source                    = "./modules/eks"
+  depends_on                = [module.vpc]
+  cluster_name              = var.cluster_name
+  tags                      = var.tags
+  eks_cluster_role_arn      = module.iam-cluster.eks_cluster_role_arn
+  eks_nodegroup_role_arn    = module.iam-cluster.eks_nodegroup_role_arn
+  vpc_id                    = module.vpc.vpc_id
+  subnet_ids                = flatten([module.vpc.public_subnet_ids, module.vpc.private_subnet_ids])
+  node_group_desired_size   = var.node_group_desired_size
+  node_group_max_size       = var.node_group_max_size
+  node_group_min_size       = var.node_group_min_size
+  node_group_instance_types = var.node_group_instance_types
+  private_subnet_cidrs      = module.vpc.private_subnet_cidrs
 }
 
 module "iam-cluster" {
-    source = "./modules/iam-cluster"
-    tags = var.tags
-    eks_cluster_role_name = var.eks_cluster_role_name
-    eks_nodegroup_role_name = var.eks_nodegroup_role_name
-    eks_nodegroup_instance_profile_name = var.eks_nodegroup_instance_profile_name
+  source                              = "./modules/iam-cluster"
+  tags                                = var.tags
+  eks_cluster_role_name               = var.eks_cluster_role_name
+  eks_nodegroup_role_name             = var.eks_nodegroup_role_name
+  eks_nodegroup_instance_profile_name = var.eks_nodegroup_instance_profile_name
 }
 
 module "ecr-pull-iam" {
-    source = "./modules/ecr-pull-iam"
-    project_name = var.project_name
-    eks_OIDC = module.eks.oidc_issuer_url
-    namespace = var.namespace
-    service_account_name = var.ecr_pull_service_account_name
+  source               = "./modules/ecr-pull-iam"
+  project_name         = var.project_name
+  eks_OIDC             = module.eks.oidc_issuer_url
+  namespace            = var.namespace
+  service_account_name = var.ecr_pull_service_account_name
 }
 
 module "loki-irsa" {
-    source = "./modules/loki-irsa"
-    project_name = var.project_name
-    eks_OIDC = module.eks.oidc_issuer_url
-    namespace = var.namespace
-    service_account_name = var.loki_service_account_name
-    bucket_name = module.s3.bucket_name1
+  source               = "./modules/loki-irsa"
+  project_name         = var.project_name
+  eks_OIDC             = module.eks.oidc_issuer_url
+  namespace            = var.namespace
+  service_account_name = var.loki_service_account_name
+  bucket_name          = module.s3.bucket_name1
 }
 
 module "github-oidc" {
-    source = "./modules/github-oidc"
+  source = "./modules/github-oidc"
 }
 
 module "alb" {
-    source = "./modules/alb"
-    eks_OIDC = module.eks.oidc_issuer_url
-    namespace = var.alb_namespace
-    cluster_name = var.cluster_name
-    region = var.region
-    service_account_name = var.alb_service_account_name
+  source               = "./modules/alb"
+  eks_OIDC             = module.eks.oidc_issuer_url
+  namespace            = var.alb_namespace
+  cluster_name         = var.cluster_name
+  region               = var.region
+  service_account_name = var.alb_service_account_name
 }
 
 module "argocd" {
-    source = "./modules/argocd"
-    depends_on = [ module.eks, module.alb ]
+  source     = "./modules/argocd"
+  depends_on = [module.eks, module.alb]
 }
 
 module "argocd-bootstrap" {
-    source = "./modules/argocd-bootstrap"
-    depends_on = [ module.eks, module.argocd ]
+  source     = "./modules/argocd-bootstrap"
+  depends_on = [module.eks, module.argocd]
 }
 
 
