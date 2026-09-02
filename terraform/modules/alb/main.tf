56c8a4f04ca043e2ea3b013042a5e74344a7da1b
Areeb Chowdhury <areebchowdhury05@gmail.com>
2026-09-02 19:18:05 +0100

    Updated ALB IAM

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -1,26 +1,58 @@
 data "aws_caller_identity" "current" {}
 
+locals {
+  eks_oidc_hostpath = replace(var.eks_OIDC, "https://", "")
+  eks_oidc_url      = "https://${local.eks_oidc_hostpath}"
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  url = local.eks_oidc_url
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+}
+
+data "aws_iam_policy_document" "alb_irsa_assume_role" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sts:AssumeRoleWithWebIdentity"
+    ]
+
+    principals {
+      type = "Federated"
+
+      identifiers = [
+        aws_iam_openid_connect_provider.eks.arn
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${local.eks_oidc_hostpath}:sub"
+
+      values = [
+        "system:serviceaccount:${var.namespace}:${var.service_account_name}"
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${local.eks_oidc_hostpath}:aud"
+
+      values = [
+        "sts.amazonaws.com"
+      ]
+    }
+  }
+}
+
 resource "aws_iam_role" "alb_irsa" {
   name = "alb-irsa-role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${var.eks_OIDC}"
-        },
-        Action = "sts:AssumeRoleWithWebIdentity",
-        Condition = {
-          StringEquals = {
-            "${var.eks_OIDC}:sub" = "system:serviceaccount:${var.namespace}:${var.service_account_name}"
-            "${var.eks_OIDC}:aud" = "sts.amazonaws.com"
-          }
-        }
-      }
-    ]
-  })
+  assume_role_policy = data.aws_iam_policy_document.alb_irsa_assume_role.json
 }
 
 resource "aws_iam_policy" "alb_controller_policy" {
@@ -38,7 +70,6 @@ resource "kubernetes_service_account" "alb_sa" {
   metadata {
     name      = var.service_account_name
     namespace = var.namespace
-    
 
     annotations = {
       "eks.amazonaws.com/role-arn" = aws_iam_role.alb_irsa.arn
